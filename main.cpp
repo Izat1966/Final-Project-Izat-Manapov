d1234e4df5b2205445b14f98eb98dcf1d9996234
Izat1966 <150505243+Izat1966@users.noreply.github.com>
2024-05-13 21:01:51 +0500

    Main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,46 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-    cout<<"Hello World";
+struct Event {
+    string name;
+    string date;
+    string time;
+    string location;
+};
 
+void displayEvent(const Event& event) {
+    cout << "Event Name: " << event.name << endl;
+    cout << "Date: " << event.date << endl;
+    cout << "Time: " << event.time << endl;
+    cout << "Location: " << event.location << endl;
+}
+
+void addEvent(Event& event) {
+    cout << "Enter event name: ";
+    getline(cin, event.name);
+    cout << "Enter event date (Date/Month/Year): ";
+    getline(cin, event.date);
+    cout << "Enter event time: ";
+    getline(cin, event.time);
+    cout << "Enter event location: ";
+    getline(cin, event.location);
+}
+
+int main() {
+    char ans ='Y';
+    
+    while (ans == 'Y' || ans == 'y') {  
+        Event EventsOfUsers;
+        addEvent(EventsOfUsers);
+        cout << "\nEvent Details:\n";
+        displayEvent(EventsOfUsers);
+        cout << "Do you want to add one more time ? Y/N: ";
+        cin >> ans;
+        cin.ignore();
+        
+    }
     return 0;
 }
